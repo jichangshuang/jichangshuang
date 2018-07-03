d63877be4bf81c26f81e49ff6a422352d06a6327
jichangshuang <863334068@qq.com>
2018-07-03 20:49:58 +0800

    Scanner控制台输入练习

---
+++ myTest.java
@@ -0,0 +1,10 @@
+public class  People {
+  public static void  mian (String []args) {
+	  int a = 10;
+	  Scanner sc = new Scanner(System.in);
+	  Syetem.out.println("请输入你的内容:");
+	  int b = sc.nextInt();
+
+  }
+
+}
